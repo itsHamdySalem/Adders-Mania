8c56f9c1172e5293086eac45772daeea474d225a
itsHamdySalem <hamdysalem112002@gmail.com>
2023-12-11 21:32:02 +0200

    Fixing a bug at the test bench;

--- src/CarryLookAheadAdder/tb_CarryLookAheadAdder.v
+++ src/CarryLookAheadAdder/tb_CarryLookAheadAdder.v
@@ -16,116 +16,96 @@ module tb_CarryLookAheadAdder;
       .Cout(Cout)
   );
 
-  // Initial values
   initial begin
-    A   = 0;
-    B   = 0;
-    Cin = 0;
-
     // Test Case 1: Overflow of positive numbers
-    A   = 2147483647;
-    B   = 1;
+    A = 2147483647;  // Maximum positive signed 32-bit integer
+    B = 1;
     Cin = 0;
     #10;
-    // Expected: F = A + B, Cout = 1
-    if (F !== (A + B) || Cout !== 1) begin
-      $display("TestCase#1: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#1: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#1: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 2: Overflow of negative numbers
-    A   = -2147483648;
-    B   = -1;
+    A = -2147483648;  // Minimum negative signed 32-bit integer
+    B = -1;
     Cin = 0;
     #10;
-    // Expected: F = A + B, Cout = 1
-    if (F !== (A + B) || Cout !== 1) begin
-      $display("TestCase#2: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#2: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#2: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 3: Addition of positive and negative number
-    A   = 100;
-    B   = -50;
+    A = 100;
+    B = -50;
     Cin = 0;
     #10;
-    // Expected: F = A + B, Cout = 0
-    if (F !== (A + B) || Cout !== 0) begin
-      $display("TestCase#3: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#3: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#3: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 4: Addition of positive and positive number
-    A   = 50;
-    B   = 30;
+    A = 50;
+    B = 30;
     Cin = 0;
     #10;
-    // Expected: F = A + B, Cout = 0
-    if (F !== (A + B) || Cout !== 0) begin
-      $display("TestCase#4: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#4: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#4: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 5: Addition of negative and negative number
-    A   = -30;
-    B   = -20;
+    A = -30;
+    B = -20;
     Cin = 0;
     #10;
-    // Expected: F = A + B, Cout = 0
-    if (F !== (A + B) || Cout !== 0) begin
-      $display("TestCase#5: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#5: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#5: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 6: Additional random test case
-    A   = 10;
-    B   = 15;
+    A = 10;
+    B = 15;
     Cin = 1;
     #10;
-    // Expected: F = A + B + Cin, Cout = 0
-    if (F !== (A + B + Cin) || Cout !== 0) begin
-      $display("TestCase#6: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#6: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#6: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 7: Additional random test case
-    A   = -40;
-    B   = 25;
+    A = -40;
+    B = 25;
     Cin = 0;
     #10;
-    // Expected: F = A + B, Cout = 0
-    if (F !== (A + B) || Cout !== 0) begin
-      $display("TestCase#7: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#7: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#7: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
     // Test Case 8: Additional random test case
-    A   = 0;
-    B   = 0;
+    A = -10;
+    B = -5;
     Cin = 1;
     #10;
-    // Expected: F = A + B + Cin, Cout = 0
-    if (F !== (A + B + Cin) || Cout !== 0) begin
-      $display("TestCase#8: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F,
-               Cout);
+    if ({Cout, F} !== (A + B + Cin)) begin
+      $display("TestCase#8: failed with input A=%0d, B=%0d and Output F=%0d, Cout=%0d", A, B, F, Cout);
     end else begin
       $display("TestCase#8: success. Input A=%0d, B=%0d, Output F=%0d", A, B, F);
     end
 
+    $stop;
   end
 
 endmodule
